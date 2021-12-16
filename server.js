4dabdcf3efdb2aefa69e88f3b08d6c994c997861
Debbiche-Senda <debbiche.senda.douda@gmail.com>
2021-12-16 14:08:02 +0100

    Server connection using express

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config({path: "./config/.env"});
+const express = require('express');
+
+const app = express();
+
+
+app.use(express.json());
+
+app.listen(process.env.PORT, (err)=>{
+    err ? console.log("Server connection failed", err) : console.log("Server connected successfully");
+});
+
+
+
+
